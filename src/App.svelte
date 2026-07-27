65ae9bf5786ef9f9f2726b7581dcfc0f8f58ef02
Khanh <ngiakhanh18@gmail.com>
2026-07-27 17:53:07 -0500

    first?

---
+++ src/App.svelte
@@ -0,0 +1,68 @@
+ <div class="bg-white w-full border-stone-200 py-4 flex gap-5 flex-col items-center justify-center">
+     <span class="text-[15px]">
+       HOMEGOOD <span style="color: #d37439;">PRODUCTS</span>
+     </span>
+     <div class="flex-row flex gap-10 text-[11px]  text-stone-500">
+         <span>Kitchen</span>
+         <span>Yard</span>
+         <span>Garage</span>
+     </div>
+ </div>
+<div class="w-full h-full flex items-center justify-center font-mono flex-col sm:flex-row mt-10 gap-10 ">
+
+    <div class="border border-stone-200 flex justify-center items-center w-76 h-70 rounded-md flex-col">
+        <div class="h-70 justify-center items-center flex">
+            <model stagemode="orbit"  alt="A model of a teapot" class="bg-[#F4F2ED]">
+                <source src="/MugDragon.usdz" type="model/vnd.usdz+zip" />
+            </model>
+        </div>
+        <div class="h-30 bg-white w-full rounded-b-md flex items-center">
+            <div class="pl-4 flex flex-col gap-1.5">
+                <span class="text-[12px] font-light">Dragon Mug</span>
+                <span class="text-[10px] text-[#d37439]">$60</span>
+                <span class="inline-block w-fit text-[6.5px] px-1.5 py-1 bg-[#F4F2ED] uppercase tracking-wide text-stone-500 rounded-md">
+                    HYDRATION
+                </span>
+            </div>
+
+        </div>
+    </div>
+    <div class="border border-stone-200 flex justify-center items-center w-76 h-70 rounded-md flex-col">
+        <div class="h-70 justify-center items-center flex">
+            <model stagemode="orbit"  alt="A model of a teapot" class="bg-[#F4F2ED]">
+                <source src="/MomMug.usdz" type="model/vnd.usdz+zip" />
+            </model>
+        </div>
+        <div class="h-30 bg-white w-full rounded-b-md flex items-center">
+            <div class="pl-4 flex flex-col gap-1.5">
+                <span class="text-[12px] font-light">Best Mom Mug</span>
+                <span class="text-[10px] text-[#d37439]">$90</span>
+                <span class="inline-block w-fit text-[6.5px] px-1.5 py-1 bg-[#F4F2ED] uppercase tracking-wide text-stone-500 rounded-md">
+                    HYDRATION
+                </span>
+            </div>
+
+        </div>
+    </div>
+    <div class="border border-stone-200 flex justify-center items-center w-76 h-70 rounded-md flex-col">
+        <div class="h-70 justify-center items-center flex">
+            <model stagemode="orbit"  alt="A model of a teapot" class="bg-[#F4F2ED]">
+                <source src="/Sun.usdz" type="model/vnd.usdz+zip" />
+            </model>
+        </div>
+        <div class="h-30 bg-white w-full rounded-b-md flex items-center">
+            <div class="pl-4 flex flex-col gap-1.5">
+                <span class="text-[12px] font-light">Water Bottle</span>
+                <span class="text-[10px] text-[#d37439]">$90</span>
+                <span class="inline-block w-fit text-[6.5px] px-1.5 py-1 bg-[#F4F2ED] uppercase tracking-wide text-stone-500 rounded-md">
+                    HYDRATION
+                </span>
+            </div>
+
+        </div>
+    </div>
+
+</div>
+
+<style>
+</style>
